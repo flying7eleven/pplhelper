1ffde558f069d2e0cb8c92c4a260f21e60c6895b
Tim Hütz <tim@huetz.biz>
2014-05-30 18:49:37 +0200

    The first fragment will now be initialized after the app was started

--- app/src/main/java/com/halcyonwaves/apps/pplhelper/activities/MainActivity.java
+++ app/src/main/java/com/halcyonwaves/apps/pplhelper/activities/MainActivity.java
@@ -4,12 +4,21 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import com.halcyonwaves.apps.pplhelper.R;
+import com.halcyonwaves.apps.pplhelper.fragments.FlightTimeCalculationFragment;
 
 
 public class MainActivity extends Activity {
 	@Override
 	protected void onCreate( Bundle savedInstanceState ) {
 		super.onCreate( savedInstanceState );
-		setContentView( R.layout.activity_main );
+		this.setContentView( R.layout.activity_main );
+
+		// set the default preferences
+		//PreferenceManager.setDefaultValues( this, R.xml.prefs, false );
+
+		// if we're not resuming from a stored state, show the fragment content of the main fragment
+		if ( savedInstanceState == null ) {
+			this.getFragmentManager().beginTransaction().add( R.id.container, new FlightTimeCalculationFragment() ).commit();
+		}
 	}
 }
