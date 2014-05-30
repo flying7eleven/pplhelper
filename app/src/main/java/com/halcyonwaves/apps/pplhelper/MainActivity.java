55832296c13368944eeb621f5959b3aeb646ec32
Tim Hütz <tim@huetz.biz>
2014-05-30 17:51:09 +0200

    Added the startup activity to the project

---
+++ app/src/main/java/com/halcyonwaves/apps/pplhelper/MainActivity.java
@@ -0,0 +1,16 @@
+package com.halcyonwaves.apps.pplhelper.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.halcyonwaves.apps.pplhelper.R;
+
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
