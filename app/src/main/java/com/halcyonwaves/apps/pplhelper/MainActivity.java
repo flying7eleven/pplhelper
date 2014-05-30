701aa571f9ca89c6578e5850431fa4a3b802e4b2
Tim Hütz <tim@huetz.biz>
2014-05-30 18:18:42 +0200

    Applied an automatic reformatting rule for the code

--- app/src/main/java/com/halcyonwaves/apps/pplhelper/MainActivity.java
+++ app/src/main/java/com/halcyonwaves/apps/pplhelper/MainActivity.java
@@ -2,15 +2,14 @@ package com.halcyonwaves.apps.pplhelper.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+
 import com.halcyonwaves.apps.pplhelper.R;
 
 
 public class MainActivity extends Activity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate( Bundle savedInstanceState ) {
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.activity_main );
+	}
 }
