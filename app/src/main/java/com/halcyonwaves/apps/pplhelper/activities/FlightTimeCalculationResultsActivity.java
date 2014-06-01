187ec4fc27ba073d00bf582f04d63e9c79f365fc
Tim Hütz <tim@huetz.biz>
2014-06-01 20:45:09 +0200

    Added the activity for the results fragment

---
+++ app/src/main/java/com/halcyonwaves/apps/pplhelper/activities/FlightTimeCalculationResultsActivity.java
@@ -0,0 +1,16 @@
+package com.halcyonwaves.apps.pplhelper.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.halcyonwaves.apps.pplhelper.R;
+
+public class FlightTimeCalculationResultsActivity extends Activity {
+
+	@Override
+	protected void onCreate( Bundle savedInstanceState ) {
+		super.onCreate( savedInstanceState );
+		this.setContentView( R.layout.activity_flight_time_calculation_results );
+	}
+
+}
