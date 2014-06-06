fe8fb97354d36cd1fc638dd5c8636277a93cf5db
Tim Hütz <tim@huetz.biz>
2014-06-06 16:20:02 +0200

    Implemented a navigation dawer

--- app/src/main/java/com/halcyonwaves/apps/pplhelper/activities/FlightTimeCalculationActivity.java
+++ app/src/main/java/com/halcyonwaves/apps/pplhelper/activities/FlightTimeCalculationActivity.java
@@ -2,8 +2,16 @@ package com.halcyonwaves.apps.pplhelper.activities;
 
 import android.app.Activity;
 import android.app.FragmentTransaction;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.halcyonwaves.apps.pplhelper.FlightHours;
 import com.halcyonwaves.apps.pplhelper.R;
@@ -12,6 +20,13 @@ import com.halcyonwaves.apps.pplhelper.fragments.FlightTimeCalculationResultsFra
 
 
 public class FlightTimeCalculationActivity extends Activity implements FlightTimeCalculationInputFragment.OnFragmentInteractionListener {
+	private CharSequence mTitle;
+	private CharSequence mDrawerTitle;
+	private DrawerLayout mDrawerLayout;
+	private ListView mDrawerList;
+	private ActionBarDrawerToggle mDrawerToggle;
+	private String[] mAppCategories;
+
 	@Override
 	protected void onCreate( Bundle savedInstanceState ) {
 		super.onCreate( savedInstanceState );
@@ -20,14 +35,48 @@ public class FlightTimeCalculationActivity extends Activity implements FlightTim
 		// set the default preferences
 		PreferenceManager.setDefaultValues( this, R.xml.prefs, false );
 
+		//
+		this.mTitle = this.mDrawerTitle = this.getTitle();
+		this.mAppCategories = getResources().getStringArray( R.array.app_categories_array );
+		this.mDrawerLayout = (DrawerLayout) findViewById( R.id.drawer_layout );
+		this.mDrawerList = (ListView) findViewById( R.id.left_drawer );
+
+		// set a custom shadow that overlays the main content when the drawer opens
+		this.mDrawerLayout.setDrawerShadow( R.drawable.drawer_shadow, GravityCompat.START );
+
+		// set up the drawer's list view with items and click listener
+		this.mDrawerList.setAdapter( new ArrayAdapter<String>( this, R.layout.drawer_list_item, this.mAppCategories ) );
+		this.mDrawerList.setOnItemClickListener( new DrawerItemClickListener() );
+
+		// enable ActionBar app icon to behave as action to toggle nav drawer
+		this.getActionBar().setDisplayHomeAsUpEnabled( true );
+		this.getActionBar().setHomeButtonEnabled( true );
+
+		// ActionBarDrawerToggle ties together the the proper interactions
+		// between the sliding drawer and the action bar app icon
+		this.mDrawerToggle = new ActionBarDrawerToggle( this, this.mDrawerLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close ) {
+			public void onDrawerClosed( View view ) {
+				FlightTimeCalculationActivity.this.getActionBar().setTitle( FlightTimeCalculationActivity.this.mTitle );
+				FlightTimeCalculationActivity.this.invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+			}
+
+			public void onDrawerOpened( View drawerView ) {
+				FlightTimeCalculationActivity.this.getActionBar().setTitle( FlightTimeCalculationActivity.this.mDrawerTitle );
+				FlightTimeCalculationActivity.this.invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+			}
+		};
+		this.mDrawerLayout.setDrawerListener( this.mDrawerToggle );
+
+		// however, if we're being restored from a previous state, then we don't need to do anything and should return or else
+		// we could end up with overlapping fragments
+		if ( savedInstanceState == null ) {
+			this.selectItem( 0 );
+		}
+	}
+
+	private void selectItem( int position ) {
 		// check that the activity is using the layout version with the fragment_container FrameLayout (the one-pane layout)
 		if ( this.findViewById( R.id.fragment_container ) != null ) {
-
-			// however, if we're being restored from a previous state, then we don't need to do anything and should return or else
-			// we could end up with overlapping fragments
-			if ( savedInstanceState != null ) {
-				return;
-			}
 
 			// create a new Fragment to be placed in the activity layout
 			FlightTimeCalculationInputFragment firstFragment = FlightTimeCalculationInputFragment.newInstance();
@@ -36,8 +85,35 @@ public class FlightTimeCalculationActivity extends Activity implements FlightTim
 			firstFragment.setArguments( getIntent().getExtras() );
 
 			// add the fragment to the 'fragment_container' FrameLayout
-			this.getFragmentManager().beginTransaction().add( R.id.fragment_container, firstFragment ).commit();
+			this.getFragmentManager().beginTransaction().replace( R.id.fragment_container, firstFragment ).commit();
 		}
+
+		// update selected item and title, then close the drawer
+		this.mDrawerList.setItemChecked( position, true );
+		this.setTitle( this.mAppCategories[ position ] );
+		this.mDrawerLayout.closeDrawer( mDrawerList );
+	}
+
+	@Override
+	public void setTitle( CharSequence title ) {
+		this.mTitle = title;
+		this.getActionBar().setTitle( this.mTitle );
+	}
+
+	/**
+	 * When using the ActionBarDrawerToggle, you must call it during
+	 * onPostCreate() and onConfigurationChanged()...
+	 */
+	@Override
+	protected void onPostCreate( Bundle savedInstanceState ) {
+		super.onPostCreate( savedInstanceState );
+		this.mDrawerToggle.syncState(); // sync. the toggle state after onRestoreInstanceState has occurred.
+	}
+
+	@Override
+	public void onConfigurationChanged( Configuration newConfig ) {
+		super.onConfigurationChanged( newConfig );
+		this.mDrawerToggle.onConfigurationChanged( newConfig ); // pass any configuration change to the drawer toggles
 	}
 
 	/**
@@ -70,4 +146,10 @@ public class FlightTimeCalculationActivity extends Activity implements FlightTim
 		}
 	}
 
+	private class DrawerItemClickListener implements ListView.OnItemClickListener {
+		@Override
+		public void onItemClick( AdapterView<?> parent, View view, int position, long id ) {
+			FlightTimeCalculationActivity.this.selectItem( position );
+		}
+	}
 }
